991ab0aa548403447b21a4eb2d8811a0e014e89c
David West <david@example.com>
2020-04-21 12:37:16 +0100

    started redux store

---
+++ jotto/src/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore} from 'redux';
+//import ReduxThunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+//export const middlewares = [ReduxThunk];
+////const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+
+//export default createStoreWithMiddleware(rootReducer);
+
+export default createStore(rootReducer)
